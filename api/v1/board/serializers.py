cd8063342685e2f2ff402c8e4d91e38f5249046a
Bomi Choi <hibomi97@gmail.com>
2022-09-07 02:40:02 +0900

    Feat: 비밀번호 인증 기능 추가
    - bcrypt 라이브러리를 사용하여 암호화
    - 비밀번호가 조건에 부합하는지 검증
      - 6자리 이상 20자리 이하
      - 숫자 1개 이상 포함

--- api/v1/board/serializers.py
+++ api/v1/board/serializers.py
@@ -1,10 +1,12 @@
+import bcrypt
 from rest_framework import serializers
+from rest_framework.exceptions import PermissionDenied, ValidationError
 
 from apps.board.models import Board
 
 
 class BoardSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True, min_length=6, max_length=20, style={'input_type': 'password'})
 
     class Meta:
         model = Board
@@ -16,3 +18,38 @@ class BoardSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         )
+
+    def validate(self, attrs):
+        """ 비밀번호를 검증합니다. """
+
+        if not any(c.isdigit() for c in attrs['password']):
+            raise ValidationError({'password', '비밀번호는 숫자를 1개 이상 포함해야 합니다.'})
+        return attrs
+
+    def create(self, validated_data):
+        """ 비밀번호를 암호화한 후 게시물을 생성합니다. """
+
+        # 비밀번호 암호화
+        pw = validated_data.pop('password').encode('utf-8')
+        hashed_pw = bcrypt.hashpw(pw, bcrypt.gensalt())
+        decoded_pw = hashed_pw.decode('utf-8')
+
+        # 게시물 생성
+        post = Board.objects.create(password=decoded_pw, **validated_data)
+        return post
+
+    def update(self, instance, validated_data):
+        """ 비밀번호가 일치하는지 확인한 후 게시물을 수정합니다. """
+
+        input_pw = validated_data['password'].encode('utf-8')
+        pw = instance.password.encode('utf-8')
+
+        # 비밀번호가 일치하지 않을 경우
+        if not bcrypt.checkpw(input_pw, pw):
+            raise PermissionDenied('비밀번호가 일치하지 않습니다.')
+
+        # 게시물 수정
+        instance.title = validated_data['title']
+        instance.content = validated_data['content']
+        instance.save()
+        return instance
--- api/v1/board/views.py
+++ api/v1/board/views.py
@@ -1,4 +1,9 @@
+import bcrypt
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
 
 from apps.board.models import Board
 from .serializers import BoardSerializer
@@ -12,3 +17,18 @@ class BoardView(ListCreateAPIView):
 class BoardDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        """ 비밀번호가 일치하는지 확인한 후 게시물을 삭제합니다. """
+        
+        post = get_object_or_404(Board, id=kwargs['pk'])
+        input_pw = request.data['password'].encode('utf-8')
+        pw = post.password.encode('utf-8')
+
+        # 비밀번호가 일치하지 않을 경우
+        if not bcrypt.checkpw(input_pw, pw):
+            raise PermissionDenied('비밀번호가 일치하지 않습니다.')
+
+        # 게시물 삭제
+        post.delete()
+        return Response(status=200)
